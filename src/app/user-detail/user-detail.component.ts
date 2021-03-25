5e36ca444f55ca17f8057a590b13527e1fccd035
Kummey <alexander@thomas-kummerer.de>
2021-03-25 09:13:41 +0100

    collect use with id from firebase

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,27 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
 import { ActivatedRoute } from '@angular/router';
+import { User } from 'src/models/user.class';
 
 @Component({
   selector: 'app-user-detail',
   templateUrl: './user-detail.component.html',
-  styleUrls: ['./user-detail.component.scss']
+  styleUrls: ['./user-detail.component.scss'],
 })
 export class UserDetailComponent implements OnInit {
+  constructor(
+    private route: ActivatedRoute,
+    private firestore: AngularFirestore
+  ) {}
 
-  constructor(private route: ActivatedRoute) { }
- 
-userId : string = '';
-
+  userId: string = '';
+  user: User = new User ;
   ngOnInit(): void {
-    this.route.params.subscribe(params => {
-      this.userId = params.id
+    this.route.params.subscribe((params) => {
+      this.userId = params.id;
 
       console.log('The id of this route is: ', params.id);
-      console.log('Got ID' , this.userId);
-      
+      console.log('Got ID', this.userId);
+      this.getUser();
     });
   }
-
-
-
+  getUser() {
+    this.firestore
+      .collection('users')
+      .doc(this.userId)
+      .valueChanges()
+      .subscribe((user: any) => {
+        this.user = new User(user);
+        console.log('Retrieved user', this.user);
+        
+      });
+  }
 }
